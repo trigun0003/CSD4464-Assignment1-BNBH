8c404e02dcdb7d3c8c05e416f200876561b11fd9
trigun0003 <hebert54@hotmail.com>
2017-01-16 12:44:44 -0500

    "First Commit"

---
+++ src/csd4464/assignment1/bnbh/CSD4464Assignment1BNBH.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csd4464.assignment1.bnbh;
+
+/**
+ *
+ * @author c0687631
+ */
+public class CSD4464Assignment1BNBH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
